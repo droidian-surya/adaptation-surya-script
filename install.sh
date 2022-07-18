26613c8b3a76effedd86e5ef0702a916193cde31
Bardia Moshiri <fakeshell@bardia.tech>
2022-07-18 14:00:17 +0430

    add files

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mount /data/rootfs.img /mnt/
+mknod /dev/loop1 b 7 8
+losetup -d /dev/loop1
+losetup /dev/loop1 /data/rootfs.img
+mkdir /rootfs && mount /dev/loop1 /rootfs
+cp ./etc/udev/rules.d/70-surya.rules /rootfs/etc/udev/rules.d/
+cp ./etc/ofono/main.conf /rootfs/etc/ofono/
+cp -r ./etc/phosh /rootfs/etc/
+cp ./etc/systemd/system/phosh.service.d/90-wait.conf /rootfs/etc/systemd/system/phosh.service.d/
+echo "Now just reboot"
