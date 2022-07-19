5855517eede9c5405f8b9cb554649aa2b631ee3f
Bardia Moshiri <fakeshell@bardia.tech>
2022-07-19 16:57:53 +0430

    add surya repo fixes

--- install.sh
+++ install.sh
@@ -8,4 +8,6 @@ cp ./etc/udev/rules.d/70-surya.rules /rootfs/etc/udev/rules.d/
 cp ./etc/ofono/main.conf /rootfs/etc/ofono/
 cp -r ./etc/phosh /rootfs/etc/
 cp ./etc/systemd/system/phosh.service.d/90-wait.conf /rootfs/etc/systemd/system/phosh.service.d/
+cp ./etc/apt/sources.list.d/surya.list /rootfs/etc/apt/sources.list.d
+cp ./usr/share/keyrings/surya.gpg /rootfs/usr/share/keyrings/surya.gpg
 echo "Now just reboot"
