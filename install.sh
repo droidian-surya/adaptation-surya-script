ef596236df6ee63f1adf1b5ee618b2bf7bd00ccf
Bardia Moshiri <fakeshell@bardia.tech>
2023-01-06 17:59:45 -0500

    remove flashlight

--- install.sh
+++ install.sh
@@ -7,13 +7,12 @@ cp -r ./etc/udev/rules.d/70-surya.rules /rootfs/etc/udev/rules.d/
 cp -r ./etc/ofono/main.conf /rootfs/etc/ofono/
 cp -r ./etc/phosh/ /rootfs/etc/
 cp -r ./etc/systemd/system/phosh.service.d/90-wait.conf /rootfs/etc/systemd/system/phosh.service.d/
-cp -r ./etc/systemd/system/droidian-sysfs-torch.service /rootfs/etc/systemd/system/droidian-sysfs-torch.service
 cp -r ./etc/apt/sources.list.d/surya.list /rootfs/etc/apt/sources.list.d
 cp -r ./usr/share/keyrings/surya.gpg /rootfs/usr/share/keyrings/surya.gpg
 cp -r ./usr/share/applications/* /rootfs/usr/share/applications/
 cp -r ./usr/share/icons/* /rootfs/usr/share/icons/
-mkdir -p /rootfs/opt/
-cp -r ./opt/droidian-sysfs-torch/ /rootfs/opt/
 cp -r ./usr/bin/* /rootfs/usr/bin/
-chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && systemctl enable droidian-sysfs-torch'
+chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin"'
 echo "Now just reboot"
+echo "After rebooting run: sudo apt update && sudo apt upgrade -y && sudo apt install droidian-sysfs-torch -y"
+echo "to get flashlight working"
