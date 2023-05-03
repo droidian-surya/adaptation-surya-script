107a54bb4a66c591716e463474af9fa4442d4899
Bardia Moshiri <fakeshell@bardia.tech>
2023-05-03 01:46:21 -0400

    add 32to64 and start ofono after the boot finishes
    pulseaudio will try to access 32 bit libraries so we can mount 64bit versions to mitigate it

--- install.sh
+++ install.sh
@@ -5,14 +5,15 @@ mkdir /rootfs
 mount /dev/loop1 /rootfs
 cp -r ./etc/udev/rules.d/70-surya.rules /rootfs/etc/udev/rules.d/
 cp -r ./etc/ofono/main.conf /rootfs/etc/ofono/
+cp -r ./etc/ofono/ril_subscription.conf /rootfs/etc/ofono
 cp -r ./etc/phosh/ /rootfs/etc/
-cp -r ./etc/systemd/system/phosh.service.d/90-wait.conf /rootfs/etc/systemd/system/phosh.service.d/
+cp -r ./etc/systemd/system/ofono.service.d/90-wait.conf /rootfs/etc/systemd/system/ofono.service.d/
 cp -r ./etc/apt/sources.list.d/surya.list /rootfs/etc/apt/sources.list.d
 cp -r ./usr/share/keyrings/surya.gpg /rootfs/usr/share/keyrings/surya.gpg
 cp -r ./usr/share/applications/* /rootfs/usr/share/applications/
 cp -r ./usr/share/icons/* /rootfs/usr/share/icons/
 cp -r ./usr/bin/* /rootfs/usr/bin/
-chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin"'
+chroot /rootfs/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin systemctl enable 32to64"'
 echo "Now just reboot"
 echo "After rebooting run: sudo apt update && sudo apt upgrade -y && sudo apt install droidian-sysfs-torch -y"
 echo "to get flashlight working"
